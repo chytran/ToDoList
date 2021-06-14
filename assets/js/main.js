5e6b97a9388fdc782251f09b7e6ef33304755549
chytran <chytran@users.noreply.github.com>
2021-06-14 11:23:15 -0700

    Got the ability to add to do's

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 const form = document.getElementById("form");
-const formInput = document.querySelector("form__input").value;
-const formSubmit = document.querySelector("form__submit");
-const container = document.querySelector("to__do__container");
+var formSubmit = document.getElementById("form-submit");
+const container = document.querySelector(".to__do__container");
 
 // Creating elements
 const addToDo = document.createElement('button');
@@ -9,3 +8,19 @@ addToDo.classList = "add__button";
 const deleteToDo = document.createElement("button");
 deleteToDo.classList = "delete__button";
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var formInput = document.getElementById("form-input").value;
+    let information = document.createElement('information');
+
+    information.innerHTML = `
+            <span class="toDoResult">${formInput}</span>
+            <button class="add__button">+</button>
+            <button class="delete__button">-</button>
+    `;
+
+    if (form !== ''){
+        container.appendChild(information);
+    } else alert("Please enter a To Do");
+    
+})
