820109d2654268e1b68479821224befc598c97bd
chytran <chytran@users.noreply.github.com>
2021-06-14 12:26:57 -0700

    To do list complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,15 @@ addToDo.classList = "add__button";
 const deleteToDo = document.createElement("button");
 deleteToDo.classList = "delete__button";
 
+// Keep track of maximum to do one can do at once
+let count = 0;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     // Making a container for each individual to do 
-    let information = document.createElement('div');
+    let information = document.createElement('li');
+    information.classList.add('information');
     information.style.width = "100%";
     information.style.display = "flex";
     information.style.alignItems = "center";
@@ -23,30 +26,25 @@ form.addEventListener("submit", (e) => {
     var formInput = document.getElementById("form-input").value;
 
     // giving the information container html with input
-    information.innerHTML = `
-        <span class="toDoResult">${formInput}</span>
-        <div class="button__container">
-            <button class="delete__button">-</button>  
-        </div>
-    `;
+    information.innerHTML = formInput;
 
     // Add if form is not empty, otherwise, warn the user
     if (form !== ''){
-        container.appendChild(information);
-    } else alert("Please enter a To Do");
-    
+        if (count <= 9){
+            container.insertBefore(information, container.childNodes[0]);
+            formInput.value = '';
+            count++;
+        }
+         else {
+             alert("You reached the maximum To do's at the moment");
+         }
+    } else alert("Please enter a To Do");   
 })
 
 
-// // Delete to do
-// function deleteToDo (element) {
-//     element.parentNode.removeChild(element);
-// }
-
-// Get delete button from the new to do
-let deleteButton = document.querySelector('delete__button');
-
-// Delete specific to do on click
-deleteButton.addEventListener('click', (e) => {
-    e.remove();
+// Delete to do
+container.addEventListener('click', (e) => {
+    if (e.target.tagName == 'LI'){
+        e.target.classList.toggle('checked');
+    }
 })
